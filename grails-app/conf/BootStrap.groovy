39c376353dd374475d56f42edbcb3f59be6067c2
Benny Neugebauer <bn@bennyn.de>
2013-08-19 18:40:09 +0200

    Customized JSON rendering.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,18 @@
+import grails.converters.JSON
+import secret.happiness.Author
+
 class BootStrap {
 
+    // http://manbuildswebsite.com/2010/02/15/rendering-json-in-grails-part-3-customise-your-json-with-object-marshallers/
     def init = { servletContext ->
+        JSON.registerObjectMarshaller(Author) {
+            def returnArray = [:]
+            returnArray['name'] = it.name
+            returnArray['url'] = it.website
+            return returnArray
+        }
     }
+
     def destroy = {
     }
 }
