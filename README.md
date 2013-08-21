3b112b70cbb20ef8fc80e9c7c5205854f1b4e540
Benny Neugebauer <bn@bennyn.de>
2013-08-21 11:39:00 +0200

    Configured MySQL database

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 secret-happiness
 ================
+1. Create MySQL database:
+mysql -uroot -pabc123 -e "CREATE DATABASE IF NOT EXISTS coding DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_general_ci'"
+
+2. Start Grails project
+
+3. Create an "author"
+
+4. Check if the "author" is stored in the database:
+echo "USE coding; SELECT * FROM author;" | mysql -pabc123 -uroot
+
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,10 @@
+// http://www.redtoad.ca/ataylor/2011/06/getting-started-with-grails-and-mysql/
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+    username = "root"
+    password = "abc123"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -13,31 +15,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/coding?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/coding?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/coding?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
 }
